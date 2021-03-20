62030870a0587a39f16da6b6fdd7730bf219e1f3
TrungHieu-HCMUIT <19521515@gm.uit.edu.vn>
2021-03-20 15:13:57 +0700

    Customize BottomNavView & add dummy xml fragment data

---
+++ app/src/main/java/com/example/dailyrunning/Record/RecordActivity.java
@@ -0,0 +1,16 @@
+package com.example.dailyrunning.Record;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dailyrunning.R;
+
+public class RecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+    }
+}
