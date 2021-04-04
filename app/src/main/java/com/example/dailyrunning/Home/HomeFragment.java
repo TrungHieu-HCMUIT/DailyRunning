40f4c0ee6dcd86d6322af367b8962376baf159fd
TrungHieu-HCMUIT <19521515@gm.uit.edu.vn>
2021-04-04 15:43:55 +0700

    Fix tabLayout bug

--- app/src/main/java/com/example/dailyrunning/Home/HomeFragment.java
+++ app/src/main/java/com/example/dailyrunning/Home/HomeFragment.java
@@ -1,20 +1,54 @@
 package com.example.dailyrunning.Home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.annotation.LongDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.dailyrunning.R;
+import com.flyco.tablayout.SegmentTabLayout;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class HomeFragment extends Fragment {
+    private AppBarLayout appBarLayout;
+    private SegmentTabLayout tabLayout;
+
+    private String[] mTitles = {"Đang theo dõi", "Bạn"};
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        tabLayout = (SegmentTabLayout) view.findViewById(R.id.tabLayout);
+        tabLayout.setTabData(mTitles);
+
+        appBarLayout = (AppBarLayout) view.findViewById(R.id.topAppBar);
+
+        exposeTabLayoutWhenCollapsed();
+
+        return view;
+    }
+
+    private void exposeTabLayoutWhenCollapsed() {
+        appBarLayout.addOnOffsetChangedListener(
+                new AppBarStateChangeListener() {
+                    @Override
+                    public void onStateChanged(AppBarLayout appBarLayout, State state) {
+                        switch (state) {
+                            case COLLAPSED:
+                                tabLayout.setElevation(20);
+                                break;
+                        }
+                    }
+                }
+        );
     }
 }
