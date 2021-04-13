103db032dd1813f4676a13f1a6aefaba6d7e07eb
Huy <19521638@gm.uit.edu.vn>
2021-04-13 14:23:44 +0700

    update user avatar

--- app/src/main/java/com/example/dailyrunning/Home/HomeActivity.java
+++ app/src/main/java/com/example/dailyrunning/Home/HomeActivity.java
@@ -10,11 +10,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -31,10 +29,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class HomeActivity extends AppCompatActivity {
@@ -145,14 +141,14 @@ public class HomeActivity extends AppCompatActivity {
     }
     private void checkAuthenticationState()
     {
-        if(mFirebaseAuth.getCurrentUser()==null)
+        FirebaseUser mCurrentUser =mFirebaseAuth.getCurrentUser();
+        if(mCurrentUser ==null)
         {
             startActivityForResult(new Intent(this, LoginActivity.class),RC_SIGN_IN);
         }
         else
         {
-            FirebaseUser firebaseUser=mFirebaseAuth.getCurrentUser();
-            mCurrentUserRef=mFirebaseDatabase.getReference().child("UserInfo").child(firebaseUser.getUid());
+            mCurrentUserRef=mFirebaseDatabase.getReference().child("UserInfo").child(mCurrentUser.getUid());
 
             mCurrentUserRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
                 @Override
@@ -160,13 +156,13 @@ public class HomeActivity extends AppCompatActivity {
                     if(!task.isSuccessful())
                         return;
                     DataSnapshot taskRes=task.getResult();
-                    mCurrentUser=taskRes.getValue(UserInfo.class);
-                    if(mCurrentUser==null)
+                    HomeActivity.this.mCurrentUser =taskRes.getValue(UserInfo.class);
+                    if(HomeActivity.this.mCurrentUser ==null)
                         return;
-                    Log.v("UserGreet","Email: "+mCurrentUser.getEmail()+"\nID= "+mCurrentUser.getUserID());
+                    Log.v("UserGreet","Email: "+ HomeActivity.this.mCurrentUser.getEmail()+"\nID= "+ HomeActivity.this.mCurrentUser.getUserID());
                 }
             });
-            if(firebaseUser!=null &&!firebaseUser.isEmailVerified())
+            if(!mCurrentUser.isEmailVerified())
             {
                 showEmailVerificationDialog();
             }
@@ -183,18 +179,7 @@ public class HomeActivity extends AppCompatActivity {
     //region firebase database
     private void setUpDatabase()
     {
-        mUserInfoRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                //updateUI
 
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
     //endregion
 
--- app/src/main/java/com/example/dailyrunning/User/UserFragment.java
+++ app/src/main/java/com/example/dailyrunning/User/UserFragment.java
@@ -1,5 +1,7 @@
 package com.example.dailyrunning.User;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -19,31 +21,54 @@ import com.example.dailyrunning.R;
 import com.example.dailyrunning.Utils.MedalAdapter;
 import com.flyco.tablayout.SegmentTabLayout;
 import com.flyco.tablayout.listener.OnTabSelectListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.taosif7.android.ringchartlib.RingChart;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserFragment extends Fragment  {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
+
+public class UserFragment extends Fragment {
+    private static final int RC_PHOTO_PICKER = 101;
+    private static final String EMAIL_PROVIDER_ID="password";
     private FirebaseAuth mFirebaseAuth;
     private RecyclerView mMedalRecycleView;
     private View rootView;
     private SegmentTabLayout tab_layout;
     private ViewPager2 statisticalViewPager2;
     private RingChart mRingChart;
+    private CircleImageView avatarView;
+    private TextView userTextView;
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mAvatarStorageReference;
+    private FirebaseUser mCurrentUser;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, container, false);
-        rootView=view;
+        rootView = view;
         mFirebaseAuth = FirebaseAuth.getInstance();
-        TextView userTextView=(TextView) view.findViewById(R.id.user_textView);
+        mFirebaseStorage=FirebaseStorage.getInstance();
+        mAvatarStorageReference=mFirebaseStorage.getReference().child("avatar_photos");
+        mCurrentUser=mFirebaseAuth.getCurrentUser();
+        findView();
 
         userTextView.setOnClickListener(v -> {
             mFirebaseAuth.signOut();
         });
 
+        setUpUpdateAvatar();
 
         setUpRingChart();
         setUpMedalRecycleView();
@@ -52,14 +77,42 @@ public class UserFragment extends Fragment  {
         return view;
     }
 
-    private void setUpRingChart()
-    {
-        mRingChart=rootView.findViewById(R.id.chart_concentric);
+    private void setUpUpdateAvatar() {
+        avatarView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mCurrentUser.getProviderId().equals(EMAIL_PROVIDER_ID)) { //người dùng đăng nhập bằng email mới set avatar được
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("image/jpeg");
+                    intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                    startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+                }
+                else // nếu không thì lấy avatar link với tài khoản facebook hoặc google của người dùng
+                {
+
+                }
+            }
+        });
+    }
+
+
+    private void findView() {
+        userTextView = (TextView) rootView.findViewById(R.id.user_textView);
+
+        mRingChart = rootView.findViewById(R.id.chart_concentric);
+        tab_layout = rootView.findViewById(R.id.tl_2);
+        statisticalViewPager2 = rootView.findViewById(R.id.statistical_viewPager2);
+        mMedalRecycleView = rootView.findViewById(R.id.medal_recycleView);
+
+    }
+
+
+    private void setUpRingChart() {
         mRingChart.showLabels(false);
         mRingChart.startAnimateLoading();
 
 
-        new CountDownTimer(3000,1000){
+        new CountDownTimer(3000, 1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
@@ -75,12 +128,9 @@ public class UserFragment extends Fragment  {
 
     }
 
-    private void setUpTabLayout()
-    {
-        tab_layout=rootView.findViewById(R.id.tl_2);
-        tab_layout.setTabData(new String[]{"Theo tuần","Theo tháng","Theo năm"});
-        statisticalViewPager2=rootView.findViewById(R.id.statistical_viewPager2);
-        StatisticalViewPagerAdapter statisticalViewPagerAdapter=new StatisticalViewPagerAdapter(this);
+    private void setUpTabLayout() {
+        tab_layout.setTabData(new String[]{"Theo tuần", "Theo tháng", "Theo năm"});
+        StatisticalViewPagerAdapter statisticalViewPagerAdapter = new StatisticalViewPagerAdapter(this);
         statisticalViewPager2.setAdapter(statisticalViewPagerAdapter);
         tab_layout.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
@@ -93,7 +143,7 @@ public class UserFragment extends Fragment  {
 
             }
         });
-        ViewPager2.OnPageChangeCallback pageChangeCallback=new ViewPager2.OnPageChangeCallback() {
+        ViewPager2.OnPageChangeCallback pageChangeCallback = new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
                 super.onPageScrolled(position, positionOffset, positionOffsetPixels);
@@ -113,12 +163,11 @@ public class UserFragment extends Fragment  {
         statisticalViewPager2.registerOnPageChangeCallback(pageChangeCallback);
 
     }
-    private void setUpMedalRecycleView()
-    {
-        mMedalRecycleView = rootView.findViewById(R.id.medal_recycleView);
-        mMedalRecycleView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL,false));
 
-        List<Integer> medalIDs=new ArrayList<>();
+    private void setUpMedalRecycleView() {
+        mMedalRecycleView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+
+        List<Integer> medalIDs = new ArrayList<>();
         medalIDs.add(R.drawable.medal_1);
         medalIDs.add(R.drawable.medal_2);
         medalIDs.add(R.drawable.medal_3);
@@ -135,7 +184,25 @@ public class UserFragment extends Fragment  {
         medalIDs.add(R.drawable.medal_4);
         medalIDs.add(R.drawable.medal_5);
 
-        MedalAdapter adapter=new MedalAdapter(medalIDs);
+        MedalAdapter adapter = new MedalAdapter(medalIDs);
         mMedalRecycleView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode==RC_PHOTO_PICKER && resultCode==RESULT_OK)
+        {
+            Uri selectedImageUri = data.getData();
+            StorageReference photoRef=mAvatarStorageReference.child(selectedImageUri.getLastPathSegment());
+            photoRef.putFile(selectedImageUri).addOnSuccessListener( new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri userAvatarUri= taskSnapshot.getUploadSessionUri();
+                    UserProfileChangeRequest newProfile =new UserProfileChangeRequest.Builder().setPhotoUri(userAvatarUri).build();
+
+                }
+            });
+        }
     }
 }
