3c8df64073d96c3a558eb86531c8eee88d04959c
Huy <19521638@gm.uit.edu.vn>
2021-06-18 20:36:30 +0700

    splash screen demo

---
+++ app/src/main/java/com/example/dailyrunning/splashScreen/SplashActivity.java
@@ -0,0 +1,98 @@
+package com.example.dailyrunning.splashScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.ViewPropertyAnimatorCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.dailyrunning.R;
+import com.example.dailyrunning.home.HomeActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    public static final int STARTUP_DELAY = 300;
+    public static final int ANIM_ITEM_DURATION = 1000;
+    public static final int ITEM_DELAY = 300;
+
+    private boolean animationStarted = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setTheme(R.style.Theme_DailyRunning);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+
+        if (!hasFocus || animationStarted) {
+            return;
+        }
+
+        animate();
+
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    private void animate() {
+        ImageView logoImageView = (ImageView) findViewById(R.id.img_logo);
+        //ViewGroup container = (ViewGroup) findViewById(R.id.container);
+        TextView appName=findViewById(R.id.textView);
+
+        ViewCompat.animate(logoImageView)
+                .translationY(-250)
+                .setStartDelay(STARTUP_DELAY).withEndAction(
+                () -> {
+
+                 })
+                .setDuration(ANIM_ITEM_DURATION).setInterpolator(
+                new DecelerateInterpolator(1.2f)).start();
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.zoom_in);
+        logoImageView.startAnimation(animation);
+        ViewPropertyAnimatorCompat viewAnimator = ViewCompat.animate(appName)
+                .scaleY(1).scaleX(1)
+                .setStartDelay((ITEM_DELAY) + 500)
+                .setDuration(500);
+        viewAnimator.setInterpolator(new DecelerateInterpolator()).start();
+        Intent intent =new Intent(this, HomeActivity.class);
+
+
+        (new Handler()).postDelayed(() -> {
+            startActivity(intent);
+        },2000);
+
+/*
+        for (int i = 0; i < container.getChildCount(); i++) {
+            View v = container.getChildAt(i);
+            ViewPropertyAnimatorCompat viewAnimator;
+
+            if (!(v instanceof Button)) {
+                viewAnimator = ViewCompat.animate(v)
+                        .translationY(50).alpha(1)
+                        .setStartDelay((ITEM_DELAY * i) + 500)
+                        .setDuration(1000);
+            } else {
+                viewAnimator = ViewCompat.animate(v)
+                        .scaleY(1).scaleX(1)
+                        .setStartDelay((ITEM_DELAY * i) + 500)
+                        .setDuration(500);
+            }
+
+            viewAnimator.setInterpolator(new DecelerateInterpolator()).start();
+        }*/
+    }
+}
