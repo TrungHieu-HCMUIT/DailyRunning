d12f3ad975de9bebb8bcf650fc68181680bddeae
khoale38 <kevinle38@gmail.com>
2021-11-13 12:55:18 +0700

    done test login and register

--- app/src/androidTest/java/com/example/dailyrunning/user/RegisterActivityTest.java
+++ app/src/androidTest/java/com/example/dailyrunning/user/RegisterActivityTest.java
@@ -1,22 +1,37 @@
 package com.example.dailyrunning.user;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static com.example.dailyrunning.authentication.LoginActivityTest.clickXY;
+import static com.example.dailyrunning.authentication.LoginActivityTest.test_Logout;
+
+import static org.hamcrest.Matchers.not;
+
 import android.os.SystemClock;
+import android.widget.DatePicker;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.contrib.PickerActions;
+
 
 import com.example.dailyrunning.R;
 import com.example.dailyrunning.authentication.LoginActivityTest;
 import com.example.dailyrunning.home.HomeActivity;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class RegisterActivityTest {
@@ -37,9 +52,9 @@ public class RegisterActivityTest {
     final String correctPassword = "Password1";
     final String incorrectPassword = "pass1";
     final String correctName= "Test";
-    /*//region isValidatePasswordTest
 
 
+    //region isValidatePasswordTest
 
     @Test
     public void test_password1() {
@@ -177,7 +192,7 @@ public class RegisterActivityTest {
         onView(withId(R.id.registerRetypePassword)).perform(replaceText(incorrectPassword));
     }
 
-    //endregion*/
+    //endregion
 
     //region validateData
     @Test
@@ -323,7 +338,276 @@ public class RegisterActivityTest {
 
     }
 
-    //end region
+    //endregion
+
+    //region onregisterclick
+
+
+    @Test
+    public void test_registerNormalCase() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+        onView(withId(R.id.registerEmail)).perform(replaceText("test134@gmail.com"));
+        onView(withId(R.id.registerPassword)).perform(replaceText(correctPassword));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(correctPassword));
+        onView(withId(R.id.registerName)).perform(replaceText("Testing123"));
+        onView(withId(R.id.register_button)).perform(click());
+        SystemClock.sleep(3000);
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_registerNullEmailPwdRePwd() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+
+        onView(withId(R.id.registerName)).perform(replaceText(correctName));
+        onView(withId(R.id.registerEmail)).perform(replaceText(""));
+        onView(withId(R.id.registerPassword)).perform(replaceText(""));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(""));
+
+        onView(withId(R.id.register_button)).perform(click());
+        SystemClock.sleep(800);
+        onView(withText("Vui lòng điền đầy đủ thông tin!")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_registerNullNamePwd() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+
+        onView(withId(R.id.registerName)).perform(replaceText(""));
+        onView(withId(R.id.registerEmail)).perform(replaceText(correctEmail));
+        onView(withId(R.id.registerPassword)).perform(replaceText(""));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(correctPassword));
+
+        onView(withId(R.id.register_button)).perform(click());
+        SystemClock.sleep(800);
+        onView(withText("Vui lòng điền đầy đủ thông tin!")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_registerNullNamePwdWrongEmailRePwd() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+
+        onView(withId(R.id.registerName)).perform(replaceText(""));
+        onView(withId(R.id.registerEmail)).perform(replaceText(incorrectEmail));
+        onView(withId(R.id.registerPassword)).perform(replaceText(""));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(incorrectPassword));
+
+        onView(withId(R.id.register_button)).perform(click());
+        SystemClock.sleep(800);
+        onView(withText("Vui lòng điền đầy đủ thông tin!")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_registerNullRePwd() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+
+        onView(withId(R.id.registerName)).perform(replaceText(correctName));
+        onView(withId(R.id.registerEmail)).perform(replaceText(correctEmail));
+        onView(withId(R.id.registerPassword)).perform(replaceText(correctPassword));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(""));
+
+        onView(withId(R.id.register_button)).perform(click());
+        SystemClock.sleep(800);
+        onView(withText("Vui lòng điền đầy đủ thông tin!")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+    }
+
+    @Test// ko hien toast
+    public void test_registerWrongEmail() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+
+        onView(withId(R.id.registerName)).perform(replaceText(correctName));
+        onView(withId(R.id.registerEmail)).perform(replaceText(incorrectEmail));
+        onView(withText("Email không hợp lệ")).check(matches(isDisplayed()));
+        onView(withId(R.id.registerPassword)).perform(replaceText(correctPassword));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(correctPassword));
+
+
+    }
+
+    @Test
+    public void test_registerNullNameEmailWrongRePwd() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+
+        onView(withId(R.id.registerName)).perform(replaceText(""));
+        onView(withId(R.id.registerEmail)).perform(replaceText(""));
+        onView(withId(R.id.registerPassword)).perform(replaceText(correctPassword));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(incorrectPassword));
+
+        onView(withId(R.id.register_button)).perform(click());
+        SystemClock.sleep(800);
+        onView(withText("Vui lòng điền đầy đủ thông tin!")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_registerNullNameRePWdWrongEmailPwd() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+
+        onView(withId(R.id.registerName)).perform(replaceText(""));
+        onView(withId(R.id.registerEmail)).perform(replaceText(incorrectEmail));
+        onView(withId(R.id.registerPassword)).perform(replaceText(incorrectPassword));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(""));
+
+        onView(withId(R.id.register_button)).perform(click());
+        SystemClock.sleep(800);
+        onView(withText("Vui lòng điền đầy đủ thông tin!")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_registerNullNameEmailWrongPwd() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+
+        onView(withId(R.id.registerName)).perform(replaceText(""));
+        onView(withId(R.id.registerEmail)).perform(replaceText(""));
+        onView(withId(R.id.registerPassword)).perform(replaceText(incorrectPassword));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(correctPassword));
+
+        onView(withId(R.id.register_button)).perform(click());
+        SystemClock.sleep(800);
+        onView(withText("Vui lòng điền đầy đủ thông tin!")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_registerWrongPwdRePwd() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+
+        onView(withId(R.id.registerName)).perform(replaceText(correctName));
+        onView(withId(R.id.registerEmail)).perform(replaceText(correctEmail));
+        onView(withId(R.id.registerPassword)).perform(replaceText(incorrectPassword));
+        onView(withText("Mật khẩu không hợp lệ")).check(matches(isDisplayed()));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(incorrectPassword));
+
+    }
+
+    //endregion
+
+    //region clearTextBox
+    @Test
+    public void test_clearTextBox() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+
+        onView(withId(R.id.registerName)).perform(replaceText(correctName));
+        onView(withId(R.id.registerEmail)).perform(replaceText(correctEmail));
+
+        onView(withId(R.id.registerName)).perform(clearText());
+        onView(withId(R.id.registerEmail)).perform(clearText());
+
+        onView(withId(R.id.registerName)).check(matches(withText("")));
+        onView(withId(R.id.registerEmail)).check(matches(withText("")));
+
+    }
+
+    //endregion
+
+    //region togglePasswordVisible
+    @Test
+    public void test_togglePwd() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+
+        onView(withId(R.id.registerPassword)).perform(replaceText(correctPassword));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(correctPassword));
+
+        onView(withId(R.id.registerPassword))
+                .check(matches(not(ToastMatcher.isPasswordVisibilityToggleEnabled())));
+        onView(withId(R.id.registerRetypePassword))
+                .check(matches(not(ToastMatcher.isPasswordVisibilityToggleEnabled())));
+    }
+
+    //endregion
+
+    //region pickDateOfBirth
+    @Test
+    public void test_pickDateOfBirth() {
+        test_Logout();
+
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+
+        onView(withId(R.id.email_editText)).perform(typeText(correctEmail));
+        onView(withId(R.id.password_editText)).perform(replaceText(correctPassword));
+        onView(withId(R.id.login_button)).perform(click());
+        onView(withId(R.id.loading_dialog)).check(matches(isDisplayed()));
+        SystemClock.sleep(3000);
+        onView(withId(R.id.home_fragment)).check(matches(isDisplayed()));
+        onView(withId(R.id.userFragment)).perform(click());
+        onView(withId(R.id.setting_imageButton)).perform(click());
+        onView(withId(R.id.updateInfo)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2017, 6, 30));
+        SystemClock.sleep(300);
+        onView(withText("OK")).perform(click());
+
+     /*   onView(withId(R.id.updateInfo)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).check(matches(ToastMatcher.matchesDate(2017, 6, 30)));*/
+        onView(withText("30/06/2017")).check(matches(isDisplayed()));
+
+
+    }
+
+    //endregion
+
+    //region onConfirmClick
+    @Test
+    public void test_onConfirmClickNormalCase() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+        onView(withId(R.id.registerEmail)).perform(replaceText("test3@gmail.com"));
+        onView(withId(R.id.registerPassword)).perform(replaceText(correctPassword));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(correctPassword));
+        onView(withId(R.id.registerName)).perform(replaceText("Testing123"));
+        onView(withId(R.id.register_button)).perform(click());
+        SystemClock.sleep(3000);
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+        onView(withId(R.id.registerAddInfo)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2017, 6, 30));
+        SystemClock.sleep(300);
+        onView(withText("OK")).perform(click());
+        onView(withText("Xác nhận")).perform(click());
+        SystemClock.sleep(3000);
+        onView(withId(R.id.home_fragment)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_onConfirmClickAbnormalCase() {
+        test_Logout();
+        ActivityScenario scenario = ActivityScenario.launch(HomeActivity.class);
+        onView(withId(R.id.registerClickable_textView)).perform(click());
+        onView(withId(R.id.registerEmail)).perform(replaceText("test5@gmail.com"));
+        onView(withId(R.id.registerPassword)).perform(replaceText(correctPassword));
+        onView(withId(R.id.registerRetypePassword)).perform(replaceText(correctPassword));
+        onView(withId(R.id.registerName)).perform(replaceText("Testing123"));
+        onView(withId(R.id.register_button)).perform(click());
+        SystemClock.sleep(3000);
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+
+        onView(withText("Xác nhận")).perform(click());
+        onView(withText("Vui lòng nhập đầy đủ thông tin")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+
+    }
+    //endregion
+
 
 
 
--- app/src/androidTest/java/com/example/dailyrunning/user/ToastMatcher.java
+++ app/src/androidTest/java/com/example/dailyrunning/user/ToastMatcher.java
@@ -1,10 +1,17 @@
 package com.example.dailyrunning.user;
 
 import android.os.IBinder;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.DatePicker;
 
 import androidx.test.espresso.Root;
+import androidx.test.espresso.matcher.BoundedMatcher;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 
 /**
@@ -28,4 +35,32 @@ class ToastMatcher extends TypeSafeMatcher<Root> {
     public void describeTo(org.hamcrest.Description description) {
         description.appendText("is toast");
     }
+
+    public static Matcher<View> isPasswordVisibilityToggleEnabled() {
+        return new BoundedMatcher<View, TextInputLayout>(TextInputLayout.class) {
+
+            @Override public void describeTo(Description description) {
+                description.appendText("is password visibility toggle enabled");
+            }
+
+            @Override protected boolean matchesSafely(TextInputLayout view) {
+                return view.isPasswordVisibilityToggleEnabled();
+            }
+        };
+    }
+    public static Matcher<View> matchesDate(final int year, final int month, final int day) {
+        return new BoundedMatcher<View, DatePicker>(DatePicker.class) {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("matches date:");
+            }
+
+            @Override
+            protected boolean matchesSafely(DatePicker item) {
+                return (year == item.getYear() && month == item.getMonth() && day == item.getDayOfMonth());
+            }
+        };
+    }
+
 }
